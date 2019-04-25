f6cc4c7fe00cd35533b8f5ef8d22d114ee6695b1
a_zero <az.86.2020@gmail.com>
2019-04-25 19:55:10 +0800

    1.fix incloud problem. 2.finish login page(v0.0.0.0.0.1). 3.incloud and use shared_preferences to rember account info. 4.ready to use firebase_auth.

---
+++ lib/login/LoginPage.dart
@@ -0,0 +1,191 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final String RemberSet = 'rember';
+final String UsernameSet = 'rember_username';
+final String PasswordSet = 'rember_password';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final mTitle = 'Login';
+
+    return MaterialApp(
+      title: mTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(mTitle),
+        ),
+        body: LoginPageForm(),
+      ),
+    );
+  }
+}
+
+class LoginPageForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageFormState();
+  }
+}
+
+class _LoginPageFormState extends State<LoginPageForm> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  bool isLoading = false;
+  bool rember = false;
+  String username = '';
+  String password = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getRemberSet();
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: EdgeInsets.all(18.0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: usernameController,
+              validator: (string) {
+                if (string.isEmpty) return 'Username can\'t be empty.';
+              },
+              decoration: InputDecoration(
+                  labelText: 'Enter your username',
+                  hintText: 'Just can not be empty',
+                  icon: Icon(Icons.account_box)),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              child: TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                validator: (string) {
+                  if (string.isEmpty) return 'Password can\'t be empty.';
+                },
+                decoration: InputDecoration(
+                    labelText: 'Enter your password',
+                    hintText: 'Just can not be empty',
+                    icon: Icon(Icons.lock)),
+              ),
+            ),
+            Container(
+              height: 20.0,
+            ),
+            CheckboxListTile(
+              title: Text('Rember your account.'),
+              value: rember,
+              onChanged: (check) {
+                setState(() {
+                  rember = check;
+                });
+              },
+            ),
+            Container(
+              height: 20.0,
+            ),
+            RaisedButton(
+              child: Align(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+//                    crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Offstage(
+                      offstage: !isLoading,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 12.0,
+                          height: 12.0,
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.red),
+                            strokeWidth: 2.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              onPressed: () {
+                //如果验证通过
+                if (_formKey.currentState.validate()) {
+                  if (!isLoading) {
+                    falseHttp();
+                  } else {
+//                    Scaffold.of(context).showSnackBar(SnackBar(
+//                      content: Text(' you are loading, plz wait.'),
+//                    ));
+                    print('catch click event when loging... ');
+                  }
+                }
+              },
+              color: Colors.blue[400],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  //开启定时器,模拟HTTP请求.
+  void falseHttp() {
+    setState(() {
+      isLoading = true;
+    });
+    Timer(Duration(seconds: 3), () {
+      //when login success....
+      setState(() {
+        isLoading = false;
+        Scaffold.of(context).showSnackBar(SnackBar(content: Text('Logined!')));
+        setRemberSet();
+      });
+    });
+  }
+
+  //获取是否记住账号密码.
+  void getRemberSet() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    rember = sharedPreferences.getBool(RemberSet) ?? false;
+    if(rember){
+      username = sharedPreferences.getString(UsernameSet) ?? '';
+      password = sharedPreferences.getString(password) ?? '';
+      usernameController.text = username;
+      passwordController.text = password;
+    }
+  }
+
+  //登录成功之后,保存
+  void setRemberSet() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    await sp.setBool(RemberSet, rember);
+    if(rember){
+      await sp.setString(UsernameSet, usernameController.text);
+      await sp.setString(PasswordSet, passwordController.text);
+    }
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,19 +9,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:car_repair/main.dart';
+import 'package:car_repair/login/LoginPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(LoginPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//    await tester.tap(find.byIcon(Icons.add));
+//    await tester.pump();
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
